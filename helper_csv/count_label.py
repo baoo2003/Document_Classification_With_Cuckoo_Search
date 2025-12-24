062a77412c307237c787329704f6dc4df548d404
Bảo Vũ <vuquocbao673@gmail.com>
2025-12-24 11:20:12 +0700

    update: eval real data & print grid search

--- helper_csv/count_label.py
+++ helper_csv/count_label.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 # Đọc dữ liệu
-df = pd.read_csv("dataset/data_real_full.csv")
+df = pd.read_csv("dataset/data_real_full_description_clean.csv")
 
 print(df['label'].dtype)
 # ===== Chỉ lấy các dòng có description rỗng =====
 # Bao gồm cả NaN và chuỗi rỗng / toàn khoảng trắng
-df = df[
-    df["description"].isna() |
-    (df["description"].astype(str).str.strip() == "")
-]
+# df = df[
+#     df["description"].isna() |
+#     (df["description"].astype(str).str.strip() == "")
+# ]
 
 # Chỉ lấy label 6 và 8
 df = df[df["label"].isin([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])]
